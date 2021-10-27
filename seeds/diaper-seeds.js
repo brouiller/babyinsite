51e88659432687348ba83f8ad8141610d68d8663
JoeyGitsIt <josephperri3@gmail.com>
2021-10-27 12:58:05 -0400

    adding seeds again?

---
+++ seeds/diaper-seeds.js
@@ -0,0 +1,16 @@
+const { Diaper } = require("../models");
+
+const diaperData = [
+  {
+    type: "Wet",
+    time: "2016-08-09 04:05:02",
+  },
+  {
+    type: "Dry",
+    time: "2016-08-09 04:05:02",
+  },
+  {
+    type: "Soiled",
+    time: "2016-08-09 04:05:02",
+  },
+];
---
+++ seeds/user-seeds.js
@@ -0,0 +1,10 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    first_name: "Blossoming",
+    last_name: "Apricot",
+    email: "user@example.com",
+    password: "root1234",
+  },
+];
