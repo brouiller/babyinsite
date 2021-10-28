442072b17cb395c84f514053f111b567e15fe455
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-10-28 11:59:35 -0400

    tested login and signup forms for functionality, fixed related routing issues

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const sleepRoutes = require("./sleep-routes");
 const dietRoutes = require("./diet-routes");
 const diaperRoutes = require("./diaper-routes");
+const userRoutes = require("./user-routes");
 
 router.use("/sleep", sleepRoutes);
 router.use("/diet", dietRoutes);
 router.use("/diaper", diaperRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
