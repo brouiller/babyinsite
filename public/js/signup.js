152c88111a593bed60c898a33073d8f0998a08c7
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-10-28 13:07:33 -0400

    swithed some javascript from index to individual files

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+const signupFormHandler = async function (event) {
+  event.preventDefault();
+
+  const first_nameEl = document.querySelector("#first_name");
+  const last_nameEl = document.querySelector("#last_name");
+  const emailEl = document.querySelector("#email");
+  const passwordEl = document.querySelector("#password");
+
+  const response = await fetch("/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      first_name: first_nameEl.value,
+      last_name: last_nameEl.value,
+      email: emailEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to sign up");
+  }
+};
+
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
